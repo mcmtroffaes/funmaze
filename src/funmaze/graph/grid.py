707b5a77c7c9666854aa8a63002b098a8073c3e6
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2022-11-23 13:51:52 +0000

    Improve grid abstraction.

--- src/funmaze/graph/grid.py
+++ src/funmaze/graph/grid.py
@@ -1,23 +1,42 @@
-from collections.abc import Set
-from typing import Iterable
+from typing import TypeVar, Iterable
+
+import numpy as np
+import numpy.typing as npt
 
 from funmaze.graph import Edge, Graph
 
-GridNode = tuple[int, int]
+
+GridNode = TypeVar("GridNode", bound=np.generic)
 
 
-def grid_graph(size_x: int, size_y: int) -> Graph[GridNode]:
-    """Build graph on a grid."""
-    def _edges() -> Iterable[Edge[GridNode]]:
-        for x in range(size_x):
-            for y in range(size_y):
-                if x < size_x - 1:
-                    yield Edge([(x, y), (x + 1, y)])
-                if y < size_y - 1:
-                    yield Edge([(x, y), (x, y + 1)])
+def grid_sequential(shape: tuple[int, ...]) -> npt.NDArray[np.uint]:
+    seq = np.arange(0, np.product(shape), dtype=np.uint)
+    return seq.reshape(shape)
+
+
+def grid_replace_nodes(
+        positions: Iterable[tuple[int, ...]],
+        node: GridNode,
+        grid: npt.NDArray[GridNode],
+) -> npt.NDArray[GridNode]:
+    grid2 = grid.copy()
+    for pos in positions:
+        grid2[pos] = node
+    return grid2
+
+
+def neighbourhood_graph(
+        grid: npt.NDArray[GridNode]) -> Graph[GridNode]:
+    """Build neighbourhood graph based on a grid."""
+    def _edges():
+        indices = list(range(len(grid.shape)))
+        for pos, node in np.ndenumerate(grid):
+            for i in indices:
+                for delta in (-1, 1):
+                    pos2 = tuple(pos[j] + (delta if i == j else 0)
+                                 for j in indices)
+                    if all(0 <= x < m for x, m in zip(pos2, grid.shape)):
+                        node2 = grid[pos2]
+                        if node != node2:
+                            yield Edge({node, node2})
     return frozenset(_edges())
-
-
-def grid_rectangle(xs: Iterable[int], ys: Iterable[int]) -> Set[GridNode]:
-    """Build rectangular set of nodes."""
-    return frozenset((x, y) for x in xs for y in ys)
--- test/test_graph.py
+++ test/test_graph.py
@@ -1,12 +1,13 @@
-from collections.abc import Set
+import itertools
+from typing import Iterable
 
 import numpy as np
 import pytest
 
 from funmaze.graph import graph_remove_nodes, Edge, \
     graph_merge_nodes, graph_nodes
-from funmaze.graph.grid import grid_graph, grid_rectangle
-from funmaze.visualize.bitmap import graph_bitmap
+from funmaze.graph.grid import grid_sequential, neighbourhood_graph, \
+    grid_replace_nodes
 from funmaze.visualize.graphviz import graph_graphviz
 
 
@@ -31,111 +32,90 @@ def test_edge() -> None:
     assert all(len(e) == 2 for e in [e1, e2, e3])
 
 
-def test_grid_graph() -> None:
-    # 00 01
-    assert grid_graph(1, 2) == {Edge([(0, 0), (0, 1)])}
-    # 00 01
-    # 10 11
-    # 20 21
-    assert grid_graph(3, 2) == {
-        # horizontal
-        Edge([(0, 0), (0, 1)]),
-        Edge([(1, 0), (1, 1)]),
-        Edge([(2, 0), (2, 1)]),
-        # vertical
-        Edge([(0, 0), (1, 0)]), Edge([(1, 0), (2, 0)]),
-        Edge([(0, 1), (1, 1)]), Edge([(1, 1), (2, 1)]),
-    }
-
-
-def test_grid_mask() -> None:
-    rec = grid_rectangle(range(2, 5), range(1, 3))
-    assert rec == {(2, 1), (3, 1), (4, 1), (2, 2), (3, 2), (4, 2)}
+@pytest.mark.parametrize("shape,edges", [
+    # 0 1
+    ((1, 2), [(0, 1)]),
+    # 0 1
+    # 2 3
+    # 4 5
+    ((3, 2), [(0, 1), (2, 3), (4, 5), (0, 2), (2, 4), (1, 3), (3, 5)]),
+])
+def test_grid_neighbourhood_graph(
+        shape: tuple[int, ...], edges: Iterable[tuple[int, int]]) -> None:
+    grid = grid_sequential(shape)
+    graph = {Edge(edge) for edge in edges}
+    assert neighbourhood_graph(grid) == graph
 
 
 def test_grid_graph_mask() -> None:
-    grid = grid_graph(4, 4)
-    rec = grid_rectangle(range(1, 4), range(1, 4))
-    grid2 = graph_remove_nodes(grid, rec)
-    # 00 01 02 03
-    # 10 ** ** **
-    # 20 ** ** **
-    # 30 ** ** **
-    assert grid2 == {
-        Edge([(0, 0), (0, 1)]),
-        Edge([(0, 1), (0, 2)]),
-        Edge([(0, 2), (0, 3)]),
-        Edge([(0, 0), (1, 0)]),
-        Edge([(1, 0), (2, 0)]),
-        Edge([(2, 0), (3, 0)]),
+    grid = grid_sequential((4, 4))
+    graph = neighbourhood_graph(grid)
+    rec = {5, 6, 7, 9, 10, 11, 13, 14, 15}
+    graph2 = graph_remove_nodes(graph, rec)
+    # 0  1  2  3
+    # 4  ** ** **
+    # 8  ** ** **
+    # 12 ** ** **
+    assert graph2 == {
+        Edge([0, 1]), Edge([1, 2]), Edge([2, 3]),
+        Edge([0, 4]), Edge([4, 8]), Edge([8, 12]),
     }
 
 
 def test_graph_merge_nodes() -> None:
-    grid = grid_graph(4, 4)
-    rec = grid_rectangle(range(1, 4), range(1, 4))
-    grid2 = graph_merge_nodes(grid, rec, (2, 2))
-    # 00 01 02 03
-    # 10 ** ** **
-    # 20 ** 22 **
-    # 30 ** ** **
-    assert grid2 == {
-        Edge([(0, 0), (0, 1)]),
-        Edge([(0, 1), (0, 2)]),
-        Edge([(0, 2), (0, 3)]),
-        Edge([(0, 0), (1, 0)]),
-        Edge([(1, 0), (2, 0)]),
-        Edge([(2, 0), (3, 0)]),
-        Edge([(0, 1), (2, 2)]),
-        Edge([(0, 2), (2, 2)]),
-        Edge([(0, 3), (2, 2)]),
-        Edge([(1, 0), (2, 2)]),
-        Edge([(2, 0), (2, 2)]),
-        Edge([(3, 0), (2, 2)]),
+    grid = grid_sequential((4, 4))
+    graph = neighbourhood_graph(grid)
+    rec = {5, 6, 7, 9, 10, 11, 13, 14, 15}
+    graph2 = graph_merge_nodes(graph, rec, 10)
+    # 0  1  2  3
+    # 4  ** ** **
+    # 8  ** 10 **
+    # 12 ** ** **
+    assert graph2 == {
+        Edge([0, 1]),
+        Edge([1, 2]),
+        Edge([2, 3]),
+        Edge([0, 4]),
+        Edge([4, 8]),
+        Edge([8, 12]),
+        Edge([1, 10]),
+        Edge([2, 10]),
+        Edge([3, 10]),
+        Edge([4, 10]),
+        Edge([8, 10]),
+        Edge([12, 10]),
     }
 
 
 def test_graphviz(tmp_path) -> None:
-    rec1 = grid_rectangle(range(1, 4), range(1, 4))
-    rec2 = grid_rectangle(range(3, 5), range(3, 5))
-    top1 = grid_graph(7, 7)
-    top2 = graph_merge_nodes(top1, rec1, (2, 2))
-    top = graph_remove_nodes(top2, rec2)
-    nodes = graph_nodes(top)
-    names = {node: f"{i + 1}" for i, node in enumerate(sorted(nodes))}
-    positions = {node: (node[1], -node[0]) for node in nodes}
-    gv = graph_graphviz(top, names, positions)
-    assert '1 [pos="0,0!"]' in gv.source
-    assert '1 -- 2' in gv.source
-    gv2 = graph_graphviz(top, names)
-    assert '1 [pos="0,0!"]' not in gv2.source
-    assert '1 -- 2' in gv2.source
+    grid = grid_replace_nodes(
+        itertools.product(range(3, 5), range(3, 5)),
+        np.uint(99),
+        grid_replace_nodes(
+            itertools.product(range(1, 4), range(1, 4)),
+            np.uint(8),
+            grid_sequential((7, 7))))
+    graph = graph_remove_nodes(neighbourhood_graph(grid), {np.uint(99)})
+    nodes = graph_nodes(graph)
+    names = {node: str(node) for node in sorted(nodes)}
+    positions = {
+        node: (pos[1], -pos[0]) for pos, node in np.ndenumerate(grid)}
+    gv = graph_graphviz(graph, names, positions)
+    assert '0 [pos="0,0!"]' in gv.source
+    assert '0 -- 1' in gv.source
+    # for debugging:
+    # gv.render(tmp_path / "graph", view=True)
+    gv2 = graph_graphviz(graph, names)
+    assert '0 [pos="0,0!"]' not in gv2.source
+    assert '0 -- 1' in gv2.source
+    # for debugging:
+    # gv2.render(tmp_path / "graph", view=True)
 
 
 def test_graphviz_2(tmp_path) -> None:
-    top = grid_graph(2, 2)
-    nodes = graph_nodes(top)
+    grid = grid_sequential((2, 2))
+    graph = neighbourhood_graph(grid)
+    nodes = graph_nodes(graph)
     names = {node: "oops" for node in nodes}
     with pytest.raises(ValueError):  # names not unique
-        graph_graphviz(top, names)
-
-
-def test_bitmap() -> None:
-    def _pos(node: tuple[int, int]):
-        return 1 + 2 * node[0], 1 + 2 * node[1]
-    rec1 = grid_rectangle(range(1, 4), range(1, 4))
-    rec2 = grid_rectangle(range(3, 5), range(3, 5))
-    top1 = grid_graph(7, 7)
-    top2 = graph_merge_nodes(top1, rec1, (2, 2))
-    top = graph_remove_nodes(top2, rec2)
-    nodes = graph_nodes(top)
-    positions: dict[tuple[int, int], Set[tuple[int, int]]] = {
-        node: {_pos(node)} for node in nodes}
-    positions[(2, 2)] = grid_rectangle(range(3, 8), range(3, 8))
-    colors = {node: i + 1 for i, node in enumerate(sorted(nodes))}
-    bitmap = graph_bitmap(top, positions, colors, wall_color=-1, edge_color=0)
-    assert bitmap.shape == (15, 15)
-    assert (bitmap[:, 0] == np.full((15,), -1)).all()
-    assert (bitmap[0, :] == np.full((15,), -1)).all()
-    assert (bitmap[:, 14] == np.full((15,), -1)).all()
-    assert (bitmap[14, :] == np.full((15,), -1)).all()
+        graph_graphviz(graph, names)
